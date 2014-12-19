f9c4e29c72c47b142f75eccc117a61c59e510281
gladpingvin42 <tr4zhm4il@gmail.com>
2014-12-19 22:58:16 +0100

    Refactoring
    - Split into multiple files
    - Cleared up code
    - Changed structure of action
    - Added different types of actions
    - Added repeatInterval to repeat planned actions
    - Modified website to reflect changes

--- server.js
+++ server.js
@@ -1,203 +1,71 @@
+var http = require('http');
 var express = require('express');
 var app = express();
-var exec = require('child_process').exec;
-var http = require('http');
-var fs = require('fs');
 
-var database = new Database("127.0.0.1", 5984);
+
+var Database = require("./database.js");
+database = new Database('data.txt');
+
+Action = require("./action.js");
 
 //------------------------
-// WEBSERVER
+// INITIATION
 //------------------------
-
-//FILES
-app.use("/index.html", function(req, res, next) {res.sendfile(__dirname + "/www/index.html");});
-app.use("/script.js", function(req, res, next) {res.sendfile(__dirname + "/www/script.js");});
-app.use("/style.css", function(req, res, next) {res.sendfile(__dirname + "/www/style.css");});
-
-app.use("/jquery-1.11.1.min.js", function(req, res, next) {res.sendfile(__dirname + "/www/jquery-1.11.1.min.js");});
-
-//CMD
-app.use("/cmd" ,function(req, res, next){
-	
-	var actionsRaw = JSON.parse(req.query.cmd);
-	
-	actionsRaw.forEach(function(actionRaw) {
-		action = new Action(actionRaw.command, actionRaw.id, actionRaw.delay, actionRaw.timedate);
-		action.execute(true);
-	});
-	
-	res.send("Commands:" + req.query.cmd + "<script>window.location = '/';</script>");
-});
-
-//GET PLANNED ACTIONS
-app.use("/plannedActions" ,function(req, res, next){
-	database.GetPlannedActions(function(data) {
-		res.send(data)
-	});
-});
-
-//REMOVE PLANNED ACTIONS
-app.use("/removePlannedAction" ,function(req, res, next){
-	database.RemoveFromDB(req.query.id)
-	res.send("Removing from DB")
-});
-
-//MANUALLY TRIGGER DATABSE CHECK
-app.use("/DB-check" ,function(req, res, next){
-	database.Update();
-	res.send("[INFO] Database updated");
-});
-
-//REDIRECT EVERYTHING ELSE TO "/"
-app.use("/", function(req, res, next) {
-	if (req.path.length > 1) {res.redirect("/");}
-	else {res.sendfile(__dirname + "/www/index.html");}
-;});
 
 //Bind webbserver to port 80
 app.listen(80); 
 console.log("[INFO]: Application listening at port 80");
 
 //Read database from file
-database.ReadFile();
+database.readFile();
 
-//Check database every 10 sec
-setInterval( function(){database.Update()}, 10000);
+//Update database every 10 sec
+setInterval( function(){database.update()}, 10000);
 
-//------------------------
-// ACTION CLASS
-//------------------------
 
-function Action (command, id, delay, timedate) {
-
-	//Variables
-	this.command = String(command).replace(/[^onf]/g,"");
-	this.id = String(id).replace(/[^0-9|a-z|A-Z]/g,"");
-	this.delay = String(delay).replace(/[^0-9]/g,"");
-	this.timedate = String(timedate).replace(/[^0-9]/g,"");
-			
-	//Delay defaults to 0
-	if (delay == undefined) {this.delay = 0;}
-	
-	//Functions
-	this.execute = function(addToDB) {
-		if (this.timedate == "" || this.timedate < Date.now()) {
-			if (delay == 0) {
-				console.log("[CMD] tdtool --" + command + " " + id); //Log command
-				exec("tdtool --" + command + " " + id); //Execute command
-			}
-			else {
-				setTimeout(function(){
-					console.log("[CMD] tdtool --" + command + " " + id); //Log command
-					exec("tdtool --" + command + " " + id); //Execute command
-				}, (delay * 1000)); //Times 1000 to make ms
-			}
-			return true;
-		}
-		else if (addToDB){
-			database.AddToDB(this);
-		}
-		return false;
-	}
-}
 
 //------------------------
-// Database CLASS
+// WEBSERVER
 //------------------------
 
-function Database (hostname, port) {
+//Static files
+app.use("/index.html", function(req, res, next) {res.sendfile(__dirname + "/www/index.html");});
+app.use("/script.js", function(req, res, next) {res.sendfile(__dirname + "/www/script.js");});
+app.use("/style.css", function(req, res, next) {res.sendfile(__dirname + "/www/style.css");});
+app.use("/jquery-1.11.1.min.js", function(req, res, next) {res.sendfile(__dirname + "/www/jquery-1.11.1.min.js");});
 
-	var data = [];
-	var dataChangedSinceLastUpdate = false;
+//Run or add the specified commands
+app.use("/cmd" ,function(req, res, next){
 	
-	//Functions
-	this.Update = function(){
-		this.CheckDB();
-		
-		if(dataChangedSinceLastUpdate){
-			this.WriteFile();
-			dataChangedSinceLastUpdate = false;
-		}
-	}
+	var actionsRaw = JSON.parse(req.query.cmd);
 	
-	this.CheckDB = function() { //Check if commands should be executed now
-		console.log("[DB] Database check initiated");
-		var self = this;
-		
-		var actionsToRemove = [];
-		
-		data.forEach(function(action) {
-			executed = action.execute(false);
-			if (executed) {
-				actionsToRemove.push(action.databaseId);
-			}
-		});
-		
-		actionsToRemove.forEach(function(databaseId){
-			self.RemoveFromDB(databaseId);
-		});
-		
-		console.log("[DB] Database check done");
-		}
-	
-	this.RemoveFromDB = function(databaseIdToRemove) { //Remove command with the databaseId and rev from the DB
-		var indexToRemove;
-		
-		data.forEach(function(action, actionIndex){
-			if (action.databaseId == databaseIdToRemove) {indexToRemove = actionIndex}
-		});
-		
-		data.splice(indexToRemove, 1);
-		
-		dataChangedSinceLastUpdate = true;
-		console.log("[DB] CMD removed from DB:" + databaseIdToRemove);
-	}
-	
-	this.AddToDB = function(action){ //Add an action to the DB
-		action.databaseId = this.NextDatabaseId();
-		data.push(action);
-		
-		dataChangedSinceLastUpdate = true;
-		console.log("[DB] CMD added to DB:" + JSON.stringify(action));
-	}
-	
-	this.GetPlannedActions = function(callback) { //Execute function callback with the database data as argument
-		callback(JSON.stringify(data));
-	}
-	
-	this.NextDatabaseId = function(){ //Return next database id
-		if (data[0] == undefined) {return 0;}
-		else {		
-			var nextDatabaseId = data[0].databaseId;
-			data.forEach(function(action, actionIndex){
-				if (action.databaseId > nextDatabaseId){nextDatabaseId = action.databaseId;}
-			});
-			return nextDatabaseId + 1;
-		}
-	}
-	
-	this.WriteFile = function(){
-		fs.writeFile('data.txt', JSON.stringify(data), 'utf8')
-		console.log("[FS] File written");
-	}
-	
-	this.ReadFile = function(){
-		fs.readFile('data.txt','utf8', function(err, fileData){
-			if (err) {
-				return console.log(err);
-			}
-			data = [];
-			
-			fileData = JSON.parse(fileData);
-			fileData.forEach(function(actionFromFile){
-				action = new Action(actionFromFile.command, actionFromFile.id, actionFromFile.delay, actionFromFile.timedate);
-				action.databaseId = actionFromFile.databaseId;
+	actionsRaw.forEach(function(actionRaw) {
+		action = new Action(actionRaw.command, actionRaw.delay, actionRaw.timedate, actionRaw.repeatInterval);
 				
-				data.push(action);
-			});
-			
-			console.log("[FS] File read");
-		})
-	}
-}
+		//If the actions isn't executed, add it to the database
+		if(!action.execute(true)){ 
+			database.addToDB(action);
+		}
+	});
+	
+	res.send("Commands:" + req.query.cmd + "<script>window.location = '/';</script>");
+});
+
+//Return the currently planned actions
+app.use("/plannedActions" ,function(req, res, next){
+	database.getPlannedActions(function(data) {
+		res.send(data);
+	});
+});
+
+//Remove specified planned action
+app.use("/removePlannedAction" ,function(req, res, next){
+	database.removeFromDB(req.query.id)
+	res.send("Removing from DB")
+});
+
+//Redirect every other request to "/"
+app.use("/", function(req, res, next) {
+	if (req.path.length > 1) {res.redirect("/");}
+	else {res.sendfile(__dirname + "/www/index.html");}
+;});
